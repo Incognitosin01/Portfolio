4c1fcd025db9d2034b00d0b2d34b5a73b176d9c1
Sumit Mahajan <incognitosin01@Sumits-MacBook-Air.local>
2023-07-03 10:39:07 +0530

    Deleted non-required files

--- Portfolio-main/portfolio/src/components/Skills/Skills.js
+++ Portfolio-main/portfolio/src/components/Skills/Skills.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./Skills.module.css";
-import {
-	CircularProgressbar,
-	buildStyles,
-	CircularProgressbarWithChildren,
-} from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
+
 
 //? Importing Logos
 import C from "../../assets/logos/C.png";
@@ -80,90 +75,6 @@ function Skills() {
 					</div>
 					<div className={styles.logoDiv}>
 						<img src={MongoDb} alt="MongoDb" className={styles.logoImg} />
-					</div>
-				</div>
-			</div>
-			<div className={styles.codingProfileDiv}>
-				<div className={styles.title}>
-					Leetcode Profile :{" "}
-					<a
-						href="https://leetcode.com/incognitosin007/"
-						className={styles.link}
-						target="_blank"
-						rel="noreferrer"
-					>
-						Incognitosin007
-					</a>
-				</div>
-				<div className={styles.questionSolved}>
-					Total Questions Solved :{" "}
-					<span style={{ color: "cyan" }}>{data?.totalSolved}</span>
-				</div>
-				<div className={styles.progressContainer}>
-					<div className={styles.progress}>
-						<CircularProgressbarWithChildren
-							value={data?.easySolved}
-							maxValue={data?.totalEasy}
-							styles={buildStyles({
-								pathTransitionDuration: 0.5,
-								transition: "stroke-dashoffset 0.5s ease 0s",
-								textSize: "14px",
-								pathColor: "#00b8a3",
-								trailColor: "rgba(0, 184, 163, 0.1)",
-							})}
-						>
-							<div>
-								<span style={{ color: "#00b8a3" }} className={styles.ques}>
-									{data?.easySolved}
-								</span>
-								/{data?.totalEasy}
-							</div>
-						</CircularProgressbarWithChildren>
-						<p style={{ color: "#00b8a3" }} className={styles.quesType}>
-							Easy
-						</p>
-					</div>
-					<div className={styles.progress}>
-						<CircularProgressbarWithChildren
-							value={data?.mediumSolved}
-							maxValue={data?.totalMedium}
-							styles={buildStyles({
-								textSize: "14px",
-								trailColor: "rgba(255,183,66, 0.1)",
-								pathColor: "rgb(255,183,66)",
-							})}
-						>
-							<div>
-								<span style={{ color: "cyan" }} className={styles.ques}>
-									{data?.mediumSolved}
-								</span>
-								/{data?.totalMedium}
-							</div>
-						</CircularProgressbarWithChildren>
-						<p style={{ color: "cyan" }} className={styles.quesType}>
-							Medium
-						</p>
-					</div>
-					<div className={styles.progress}>
-						<CircularProgressbarWithChildren
-							value={data?.hardSolved}
-							maxValue={data?.totalHard}
-							styles={buildStyles({
-								textSize: "14px",
-								pathColor: "#ef4743",
-								trailColor: "rgba(239, 70, 67, 0.1)",
-							})}
-						>
-							<div>
-								<span style={{ color: "#ef4743" }} className={styles.ques}>
-									{data?.hardSolved}
-								</span>
-								/{data?.totalHard}
-							</div>
-						</CircularProgressbarWithChildren>
-						<p style={{ color: "#ef4743" }} className={styles.quesType}>
-							Hard
-						</p>
 					</div>
 				</div>
 			</div>
